313783e7e5e1f4e456fef451a28f2be4045e5034
fromseungjun <fromseungjun@gmail.com>
2024-05-28 21:26:18 +0900

    ver 1.0.3 // change menu

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,61 +2,8 @@
 import { z, defineCollection } from "astro:content";
 
 // 2. Define a `type` and `schema` for each collection
-const muses = defineCollection({
-  type: "content", // v2.5.0 and later
-  schema: z.object({
-    title: z.string(),
-    tags: z.array(z.string()),
-    author: z.string(),
-    description: z.string(),
-    image: z.object({
-      src: z.string(),
-      alt: z.string(),
-      positionx: z.string().optional(),
-      positiony: z.string().optional()
-    }).optional(),
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-  }),
-});
 
 const short_form = defineCollection({
-  type: "content", // v2.5.0 and later
-  schema: z.object({
-    title: z.string(),
-    tags: z.array(z.string()),
-    author: z.string(),
-    description: z.string(),
-    image: z.object({
-      src: z.string(),
-      alt: z.string(),
-      positionx: z.string().optional(),
-      positiony: z.string().optional()
-    }).optional(),
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-  }),
-});
-
-const long_form = defineCollection({
-  type: "content", // v2.5.0 and later
-  schema: z.object({
-    title: z.string(),
-    tags: z.array(z.string()),
-    author: z.string(),
-    description: z.string(),
-    image: z.object({
-      src: z.string(),
-      alt: z.string(),
-      positionx: z.string().optional(),
-      positiony: z.string().optional()
-    }).optional(),
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-  }),
-});
-
-const zeitweilig = defineCollection({
   type: "content", // v2.5.0 and later
   schema: z.object({
     title: z.string(),
@@ -94,4 +41,4 @@ const authors = defineCollection({
 
 
 // 3. Export a single `collections` object to register your collection(s)
-export const collections = { muses, short_form, long_form, zeitweilig, authors };
+export const collections = {short_form, authors };
