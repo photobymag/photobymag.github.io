3ee3f68c2e501a63bfc6280a6070b5140d95e41d
photobymag <hello@snjnpark.kr>
2024-06-26 18:34:43 +0900

    ver 1.10.1 -- hot fix

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -205,6 +205,13 @@ declare module 'astro:content' {
   collection: "magazine";
   data: InferEntrySchema<"magazine">
 } & { render(): Render[".mdx"] };
+"2024-06-26-gray-malin.mdx": {
+	id: "2024-06-26-gray-malin.mdx";
+  slug: "gray-malin";
+  body: string;
+  collection: "magazine";
+  data: InferEntrySchema<"magazine">
+} & { render(): Render[".mdx"] };
 };
 
 	};
