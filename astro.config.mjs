48b02fc8927a8aa69d79254757cbc7cdbb6f7039
photobymag <hello@snjnpark.kr>
2024-06-28 14:31:00 +0900

    ver 1.11.0 -- switch to gh-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -66,7 +66,5 @@ export default defineConfig({
     contentCollectionCache: true
     // directRenderScript: true // 주석 처리된 코드입니다.
   },
-  output: 'server',
-  adapter: deno(),
-
+  output: 'static',
 });
