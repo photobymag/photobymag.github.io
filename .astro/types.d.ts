d1f9748a24d9f6969edad985312c70bffbd02b0e
photobymag <hello@snjnpark.kr>
2024-07-03 18:44:59 +0900

    ver 1.12.0 -- todd hido

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -218,6 +218,13 @@ declare module 'astro:content' {
   collection: "magazine";
   data: InferEntrySchema<"magazine">
 } & { render(): Render[".mdx"] };
+"2024-07-02-todd-hido.mdx": {
+	id: "2024-07-02-todd-hido.mdx";
+  slug: "todd-hido";
+  body: string;
+  collection: "magazine";
+  data: InferEntrySchema<"magazine">
+} & { render(): Render[".mdx"] };
 };
 
 	};
