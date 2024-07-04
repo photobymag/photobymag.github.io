d4eab964a896903206242daba659f026f5f2b85e
photobymag <hello@snjnpark.kr>
2024-07-04 18:26:24 +0900

    ver 1.13.0 -- edward burtynsky

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -225,6 +225,13 @@ declare module 'astro:content' {
   collection: "magazine";
   data: InferEntrySchema<"magazine">
 } & { render(): Render[".mdx"] };
+"2024-07-04-edward-burtynsky.mdx": {
+	id: "2024-07-04-edward-burtynsky.mdx";
+  slug: "edward-burtynsky";
+  body: string;
+  collection: "magazine";
+  data: InferEntrySchema<"magazine">
+} & { render(): Render[".mdx"] };
 };
 
 	};
