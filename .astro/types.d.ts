de0e1f6e248fb28c0c3efeb4927f0bde2369a94d
photobymag <hello@snjnpark.kr>
2024-07-10 17:33:38 +0900

    ver 1.14.0 -- natan dvir

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -232,6 +232,13 @@ declare module 'astro:content' {
   collection: "magazine";
   data: InferEntrySchema<"magazine">
 } & { render(): Render[".mdx"] };
+"2024-07-10-natan-dvir.mdx": {
+	id: "2024-07-10-natan-dvir.mdx";
+  slug: "natan-dvir";
+  body: string;
+  collection: "magazine";
+  data: InferEntrySchema<"magazine">
+} & { render(): Render[".mdx"] };
 };
 
 	};
